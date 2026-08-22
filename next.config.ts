5700235dc073c5f3fe58a395e68945b98ae365eb
dahyeseol499 <sdhabc@gmail.com>
2026-08-23 01:52:03 +0900

    add meta tag

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://oapi.map.naver.com https://*.naver.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
